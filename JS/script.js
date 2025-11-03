3e33615b66ddb3a49137fdfffb374f1aca3a866e
SadiyaIslamSamiya <s.islam01122@gmail.com>
2025-11-04 01:29:39 +0600

    JS Nav

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,16 @@
+let main_menu = document.querySelector(".main-menu");
+
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+
+  if (window.scrollY >= 900) {
+    // main_menu.style = "background: #0c0c0c;";
+    main_menu.classList.add("s-bg");
+  } else {
+    main_menu.classList.remove("s-bg");
+  }
+});
+
 $(".counter").counterUp({
   delay: 10,
   time: 1000,
